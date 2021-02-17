bcb9898676ff29fdece634a8f619b9ccbcd0d4bf
jeffrey <jeffreyassan88@gmail.com>
2021-02-16 20:54:05 -0500

    finished Escrow test

--- test/Escrow_test.js
+++ test/Escrow_test.js
@@ -1,39 +1,66 @@
 const Escrow = artifacts.require('Escrow');
 
-const assertError= async(promise,error)=>{
-    try{
-     await promise;  
-    }catch(e){
-        assert(e.message.includes(error))
-        return;
-    }
-    assert(false);
+const assertError = async (promise, error) => {
+  try {
+    await promise;
+  } catch(e) {
+    assert(e.message.includes(error))
+    return;
+  }
+  assert(false);
 }
 
-contract('Escrow', accounts =>{
-    let escrow= null;
-    const [lawyer,payer, recipient]= accounts;
-     before(async()=>{
-         escrow= await Escrow.deployed();
-     });
+contract('Escrow', accounts => {
+  let escrow = null;
+  const [lawyer, payer, recipient] = accounts;
+  before(async () => {
+    escrow = await Escrow.deployed();
+  });
 
-it('Should deposit', async()=>{
-    await escrow.deposit({from:payer, value: 900})
-    const escrowBalance= parseInt(await web3.eth.getBalance(
-        escrow.address));
-        assert(escrowBalance=== 900);
-    });
+  it('should deposit', async () => {
+    await escrow.deposit({from: payer, value: 900});
+    const escrowBalance = parseInt(await web3.eth.getBalance(escrow.address));
+    assert(escrowBalance === 900);
+  });
 
-    it('SHould not deposit if the sender is not the payer', async()=>{
-        assertError(
-            escrow.deposit({from:accounts[5], value: 100}),
-            'Sender must be payer'
-        );
-    });
+  it('should NOT deposit if transfer exceed total escrow amount', async () => {
+    assertError(
+      escrow.deposit({from: payer, value: 1000}),
+      'Cant send more than escrow amount'
+    );
+  });
 
-    it('Should not deposit if transfer exceds amount', async()=>{
-        assertError(
-            escrow.deposit({from:payer, value:})
-        )
-    })
+  it('should NOT deposit if not sending from payer', async () => {
+    assertError(
+      escrow.deposit({from: accounts[5]}),
+      'Sender must be the payer'
+    );
+  });
+
+  it('should NOT release if full amount not received', async () => {
+    assertError(
+      escrow.release({from: lawyer}),
+      'cannot release funds before full amount is sent'
+    );
+  });
+
+  it('should NOT release if not lawyer', async () => {
+    await escrow.deposit({from: payer, value: 100}); //We are missing 100 wei
+    assertError(
+      escrow.release({from: payer}),
+      'only lawyer can release funds'
+    );
+  });
+
+  it('should release', async () => {
+    const initialRecipientBalance = web3.utils.toBN(
+      await web3.eth.getBalance(recipient)
+    );
+    await escrow.release({from: lawyer});
+    const finalRecipientBalance = web3.utils.toBN(
+      await web3.eth.getBalance(recipient)
+    );
+    assert(finalRecipientBalance.sub(initialRecipientBalance).toNumber() === 1000);
+  });
+  
 });
